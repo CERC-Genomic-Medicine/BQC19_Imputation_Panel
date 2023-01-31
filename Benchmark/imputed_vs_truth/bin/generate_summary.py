054c1c356583c436f7ca6ee912ccbc9ca2d40beb
mohadesesd <mohadese.sayahian.d@gmail.com>
2023-01-31 14:22:18 -0500

    adding comments

--- Benchmark/imputed_vs_truth/bin/generate_summary.py
+++ Benchmark/imputed_vs_truth/bin/generate_summary.py
@@ -6,10 +6,10 @@ import pandas as pd
 
 argparser = argparse.ArgumentParser(description = 'This script calculates some stats for each sample based on the comparison between imputed and truth files.')
 
-argparser.add_argument('-i', '--input_file', metavar = 'file', dest = 'in_file_name', type = str, required = True, help = 'text file containing the labels for each variant, whether it is\
- well imputed, badly imputed, missing or only present in the reference panel but in truth VCFs.')
+argparser.add_argument('-i', '--input_file', metavar = 'file', dest = 'in_file_name', type = str, required = True, help = 'text file containing the labels for each variant. labels are in [REF, WGS, WGS_0ALT, \
+REF_0ALT, WGS_0ALT_AND_REF_EQ, WGS_0ALT_AND_REF_LT, WGS_AND_REF_EQ, WGS_AND_REF_LT]')
 argparser.add_argument('-s', '--sample_ID', metavar = 'name', dest = 'in_sample_ID', type = str, required = True, help = 'ID of the sample that is used for evaluation.')
-argparser.add_argument('-o', '--output_file', metavar = 'file', dest = 'out_file_name', type = str, required = True, help = 'Output file which contains summary of numbers of different types of variant.')
+argparser.add_argument('-o', '--output_file', metavar = 'file', dest = 'out_file_name', type = str, required = True, help = 'Output file which contains summary of numbers of different types of variant and corcondance results.')
 
 def calculates_stats(sample_ID, input_file, path_out):
     input_dataframe = pd.read_csv(input_file, sep="\t", header = None)
